c61b2895ab8df20788301567916b74ca5aceee03
QiuJi <qiuji@iscas.ac.cn>
2022-04-08 16:13:25 +0800

    Port LoadFPRImm for uint64 src
     refer to: https://github.com/riscv-collab/v8/issues/535
     Also add disassemble logging for the unsupported RV64 inst.

--- src/objects/code.h
+++ src/objects/code.h
@@ -667,8 +667,8 @@ class Code : public HeapObject {
   static constexpr int kHeaderPaddingSize = (COMPRESS_POINTERS_BOOL ? 12 : 24);
 #elif V8_TARGET_ARCH_RISCV32
   // static constexpr int kHeaderPaddingSize = (COMPRESS_POINTERS_BOOL ? 12 :
-  // 24); static constexpr int kHeaderPaddingSize = (COMPRESS_POINTERS_BOOL ?
-  // 8 : 20);
+  // 24); static constexpr int kHeaderPaddingSize = (COMPRESS_POINTERS_BOOL ? 8
+  // : 20)；
   static constexpr int kHeaderPaddingSize = 12;  // RV32Gtodo
 #else
 #error Unknown architecture.
